ebcef1246460cdde2f7496d752d9ccfa4b9f9679
ashton01L <ashtonamazing@gmail.com>
2024-06-27 23:52:04 -0500

    primary operational file moved out of ignore status

---
+++ main/animal.py
@@ -0,0 +1,7 @@
+# Author: Ashton Lee
+# Github User: ashton01L
+# Date: 6/27/2024
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
